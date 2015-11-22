bf703d9756818b18b0616c5f3b728d1b65518483
thiagofalca0 <thiago_falcao07@hotmail.com>
2015-11-22 15:31:40 -0300

    push buttons with procs
    testando a chamanda de procs com o apertos dos butoes

--- push_buttons.asm
+++ push_buttons.asm
@@ -1,6 +1,10 @@
 DSEG    SEGMENT 'DATA'
 
-; add DATA
+msg1:    db      "PLAY!", 0Dh,0Ah, 24h
+msg2:    db      "PAUSE!", 0Dh,0Ah, 24h
+msg3:    db      "STOP!", 0Dh,0Ah, 24h
+msg4:    db      "UP!", 0Dh,0Ah, 24h
+msg5:    db      "DOWN!", 0Dh,0Ah, 24h
 
 DSEG    ENDS
 
@@ -35,6 +39,22 @@ NEXT:
 	MOV DX, 2080h ; input data entrada dos buttons
 	IN  AX, DX    ; 16-bit input
 	
+	CMP AL,02
+	JE  PLAY
+	
+	CMP AL,02
+	JE  PAUSE
+	
+	CMP AL,04
+	JE  STOP
+	
+	CMP AL,01
+	JE  UP
+	
+	CMP AH,01
+	JE  DOWN
+	
+	
 	MOV DX, 2040h ; output data / saida para ASCII LCD display
 	OUT DX, AL
 
@@ -44,6 +64,64 @@ NEXT:
 ; return to operating system:
     RET
 START   ENDP
+
+
+
+PLAY PROC
+    
+        mov     dx, msg1  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....  
+    RET          
+PLAY ENDP 
+
+PAUSE PROC
+    
+        mov     dx, msg2  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....  
+    RET          
+PAUSE ENDP 
+
+STOP PROC
+    
+        mov     dx, msg3  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....  
+    RET          
+STOP ENDP
+
+UP PROC
+    
+        mov     dx, msg4  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....  
+    RET          
+UP ENDP 
+
+DOWN PROC
+    
+        mov     dx, msg5  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....  
+    RET          
+DOWN ENDP 
+         
 
 ;*******************************************
 
